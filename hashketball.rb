04b8ff7346fad73c2a3711afc26a1c25a4712593
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-28 13:58:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ def game_hash
 end
 
 def all_players
-  game_hash[:home][:players].merge(game_hash[:home][:players])
+  game_hash[:home][:players].merge(game_hash[:away][:players])
 end
 
 def player_stats
