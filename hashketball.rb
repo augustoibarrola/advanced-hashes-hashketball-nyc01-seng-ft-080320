f6a78aef0cb62965f16e0a6bc7db460d26fc102f
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-28 15:05:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,10 +143,11 @@ def shoe_size(player_name)
   player_stats(player_name)[:shoe]
 end
 
-
+def all_teams
+  game_hash.values
+end
 
 def find_team(team_name)
-  all_teams = game_hash.values
   all_teams.find { |team| team[:team_name] == team_name }
 end
 
